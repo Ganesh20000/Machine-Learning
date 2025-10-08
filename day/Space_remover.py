e060e48581ce151b664c8062a3af022b0dd4a6a5
Ganesh20000 <ganeshkarli19@gmail.com>
2025-10-08 16:13:56 +0530

    add

---
+++ day/Space_remover.py
@@ -0,0 +1,9 @@
+# Input from user
+sentence = input("Enter a sentence: ")
+
+# Replace spaces with underscores
+modified_sentence = sentence.replace(" ", "_")
+
+# Output the result
+print("Modified sentence:", modified_sentence)
+
